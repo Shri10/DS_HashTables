47c518f0b88d2dc45be756689b0e5820686ab28d
Shri10 <pethe.shriraj@gmail.com>
2023-07-03 10:40:51 +0530

    first commit

---
+++ DS_HashTables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DS_HashTables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
